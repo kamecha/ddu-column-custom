bcb9df118ad4c52655af16c3960147962f7cd915
kamecha <masakikameyama81@gmail.com>
2024-01-28 01:58:40 +0900

    feature: denopsに登録したcallbackでそれぞれを設定するようにした

--- denops/@ddu-columns/custom.ts
+++ denops/@ddu-columns/custom.ts
@@ -5,16 +5,43 @@ import {
   GetTextResult,
 } from "https://deno.land/x/ddu_vim@v3.4.2/base/column.ts";
 
-export type Params = Record<never, never>;
+export type Params = {
+  getLengthCallbackId: string;
+  getTextCallbackId: string;
+};
 
 export class Column extends BaseColumn<Params> {
-  getLength({}: GetLengthArguments<Params>): Promise<number> {
-    throw new Error("Method not implemented.");
+  async getLength(args: GetLengthArguments<Params>): Promise<number> {
+    if (!this.checkParams(args.columnParams)) {
+      return Promise.resolve(0);
+    }
+    const length = await args.denops.call(
+      "denops#callback#call",
+      args.columnParams.getLengthCallbackId,
+      args,
+    );
+    return length as number;
   }
-  getText({}: GetTextArguments<Params>): Promise<GetTextResult> {
-    throw new Error("Method not implemented.");
+  async getText(args: GetTextArguments<Params>): Promise<GetTextResult> {
+    if (!this.checkParams(args.columnParams)) {
+      return Promise.resolve({
+        text: "",
+      });
+    }
+    const result = await args.denops.call(
+      "denops#callback#call",
+      args.columnParams.getTextCallbackId,
+      args,
+    );
+    return result as GetTextResult;
   }
   params(): Params {
-    throw new Error("Method not implemented.");
+    return {
+      getLengthCallbackId: "",
+      getTextCallbackId: "",
+    };
+  }
+  checkParams(params: Params): boolean {
+    return params.getLengthCallbackId !== "" && params.getTextCallbackId !== "";
   }
 }
